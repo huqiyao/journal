4f7ab9f839b9ff9a2d8f1456342c51eb9f147444
huqiyao <huqiyao@growingio.com>
2021-05-08 08:17:23 +0800

    complete part of cloudProgress

--- src/Pages/Header/CloudBtn.tsx
+++ src/Pages/Header/CloudBtn.tsx
@@ -1,8 +1,24 @@
 import React from 'react';
-import { CloudUploadOutlined} from '@ant-design/icons';
-
+import { CloudUploadOutlined } from '@ant-design/icons';
+import { Menu, Dropdown, Divider, Switch, Input } from 'antd';
+const menu = (
+  <Menu>
+    <Divider plain style={{ borderColor: 'rgb(217, 217, 217)' }}>正在上传</Divider>
+    <Menu.Item style={{textAlign:'center', fontSize:'12px',padding:'0px'}}>暂无信息</Menu.Item>
+    <Divider plain style={{ borderColor: 'rgb(217, 217, 217)' }}>已上传</Divider>
+    <Menu.Item>aa</Menu.Item>
+    <Menu.Item>aa</Menu.Item>
+  </Menu>
+);
 const CloudBtn: React.FC<{}> = ({}) => {
-  return <CloudUploadOutlined style={{marginRight: '30px',cursor:'pointer',fontSize:'16px'}} className='operate-icon'></CloudUploadOutlined>;
+  return (
+    <Dropdown overlay={menu} arrow overlayStyle={{width:'120px'}}>
+      <CloudUploadOutlined
+        style={{ marginRight: '30px', cursor: 'pointer', fontSize: '16px' }}
+        className="operate-icon"
+      ></CloudUploadOutlined>
+    </Dropdown>
+  );
 };
 
 export default CloudBtn;
